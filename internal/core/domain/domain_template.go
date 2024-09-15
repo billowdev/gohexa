e86fcd351e1224d3cdd9b8f940d27fa7bca1eec4
Akkarapon Phikulsri <akkarapon@looksocial.asia>
2024-09-15 20:15:50 +0700

    fix: improve domain template

--- internal/core/domain/domain_template.go
+++ internal/core/domain/domain_template.go
@@ -18,25 +18,18 @@ import (
 
 type {{ .FeatureName }}Domain struct {
 {{ if .UseUUID }}
-	ID                 string    ` + "`gorm:\"type:uuid;primaryKey;default:uuid_generate_v4()\" json:\"id\"`" + `
+	ID                 string    ` + "`json:\"id\"`" + `
 {{ else }}
-	ID                 uint      ` + "`gorm:\"primaryKey;autoIncrement\" json:\"id\"`" + `
+	ID                 uint      ` + "`json:\"id\"`" + `
 {{ end }}
-	CreatedAt          time.Time ` + "`json:\"created_at\" gorm:\"autoCreateTime\"`" + `
-	UpdatedAt          time.Time ` + "`json:\"updated_at\" gorm:\"autoUpdateTime\"`" + `
+	CreatedAt          time.Time ` + "`json:\"created_at\"`" + `
+	UpdatedAt          time.Time ` + "`json:\"updated_at\"`" + `
 	Field1      string    ` + "`json:\"field_1\"`" + `
-	Field2      string    ` + "`json:\"field_2\"`" + `
 }
 
 func To{{ .FeatureName }}Domain(data *models.{{ .FeatureName }}) {{ .FeatureName }}Domain {
 	if data == nil {
-		return {{ .FeatureName }}Domain{
-			{{ if .UseUUID }}
-			ID: "{{ .DefaultUUID }}",
-			{{ else }}
-			ID: 0,
-			{{ end }}
-		}
+		return {{ .FeatureName }}Domain{}
 	}
 
 	return {{ .FeatureName }}Domain{
@@ -44,7 +37,6 @@ func To{{ .FeatureName }}Domain(data *models.{{ .FeatureName }}) {{ .FeatureName
 		CreatedAt:          data.CreatedAt,
 		UpdatedAt:          data.UpdatedAt,
 		Field1:             data.Field1,
-		Field2:             defaultStringIfEmpty(data.Field2, "No Field2"),
 	}
 }
 
@@ -54,15 +46,6 @@ func To{{ .FeatureName }}Model(data {{ .FeatureName }}Domain) *models.{{ .Featur
 		CreatedAt:          data.CreatedAt,
 		UpdatedAt:          data.UpdatedAt,
 		Field1:             data.Field1,
-		Field2:             defaultStringIfEmpty(data.Field2, "No Field2"),
 	}
-}
-
-// defaultStringIfEmpty returns the default value if the input string is empty
-func defaultStringIfEmpty(value, defaultValue string) string {
-	if value == "" {
-		return defaultValue
-	}
-	return value
 }
 `
